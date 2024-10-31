24664c7b7186fcdfa9045c81c9ac7aa23c8e24fa
omiras <oscar.miras@gmail.com>
2024-10-31 18:38:01 +0100

    some tets

--- services/question.services.js
+++ services/question.services.js
@@ -2,19 +2,20 @@ const Questions = require('../models/question.model');
 
 const getRandomQuestion = async (amount) => {
 
-
-	/* const questions = await Questions.find();
-		const randomIndex = Math.floor(Math.random() * questions.length);
-		const randomQuestion = questions[randomIndex];
-		return randomQuestion; */
-
-
 	const questions = await Questions.aggregate([
 		{ $sample: { size: amount } }
 	]);
 	return questions;
 };
 
+
+const postQuestion = async (question) => {
+	// TODO
+}
+
+const getRandomQuestionsFromIA = (amount, topic) => {
+	// TODO
+}
 
 
 module.exports = {
