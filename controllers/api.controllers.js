99614a10231cb51bb62a61d05d598084a916fc04
SharmarAndrey <sharnarandrey@gmail.com>
2024-11-14 23:37:51 +0100

    Modifying the Controller(const getAiQuestions )

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -25,9 +25,9 @@ const {
  * @throws {Error} If an unexpected error occurs, a 500 status code is returned with the error message.
  */
 const getRandomQuestions = async (req, res) => {
-	try {
-		let { amount, categories } = req.query;
-		amount = parseInt(amount, 10);
+  try {
+    let { amount, categories } = req.query;
+    amount = parseInt(amount, 10);
 
     //validation of amount
     if (isNaN(amount) || amount < 1) {
@@ -36,10 +36,10 @@ const getRandomQuestions = async (req, res) => {
       amount = 30;
     }
 
-		//const randomQuestion = await getRandomQuestionsDB(amount, {categories : categories});
-		const randomQuestion = categories
-			? await getRandomQuestionsDB(amount, { categories: categories })
-			: await getRandomQuestionsDB(amount);
+    //const randomQuestion = await getRandomQuestionsDB(amount, {categories : categories});
+    const randomQuestion = categories
+      ? await getRandomQuestionsDB(amount, { categories: categories })
+      : await getRandomQuestionsDB(amount);
 
     res.status(200).json({
       message: "Random questions delivered successfully",
@@ -70,7 +70,7 @@ const getAiQuestions = async (req, res) => {
     // Log error details to the console for debugging
     console.error(
       "Error generating the questions:",
-      error.response ? error.response.data : error.message
+      /*  error.response ? error.response.data : */ error.message
     );
     // Send a 400 error response with a custom error message if generation fails
     return res.status(400).json({
