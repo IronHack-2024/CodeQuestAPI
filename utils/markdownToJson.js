b8fb82b21698507c983caf9ae5038fd3f36eb5c5
SharmarAndrey <sharnarandrey@gmail.com>
2024-11-14 22:40:42 +0100

    Preguntas por categorias

--- utils/markdownToJson.js
+++ utils/markdownToJson.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const { QUESTIONS_CATEGORIES } = require('./constants');
+const fs = require("fs");
+const { QUESTIONS_CATEGORIES } = require("./constants");
 //const fetch = require('node-fetch');
 // ** IMPORTANTE - La URL de linkedin-skill-assessments-quizzes debe ser la RAW.
 //Permite pasar parámetors por script. Ej se ejecute. node /nombre_del_archivo URL nombre_fichero_json category
@@ -9,7 +9,7 @@ const args = process.argv.slice(2);
 
 const MARKDOWN_URL = args[0];
 let titulo = args[1];
-let categories = args[2]
+let categories = args[2];
 
 // Función asíncrona para descargar el Markdown y llamar a la función que lo convierte a json y lo guarda en un archivo
 async function descargarMarkdown() {
@@ -22,7 +22,7 @@ async function descargarMarkdown() {
 
     convertiraJson(markdown);
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 }
 
@@ -31,7 +31,7 @@ function convertiraJson(markdown) {
   const questions = [];
   let match;
 
-  // Expresiones regulares para extraer acada parte del documento desde formato markdown que se utiliza en las preguntas de LinkedIn 
+  // Expresiones regulares para extraer acada parte del documento desde formato markdown que se utiliza en las preguntas de LinkedIn
 
   const titleRegex = /^## (.+)$/gm; // Obtener los títulos que en nuestra API serán las categorias (de momento  NO LO USAMOS). Es uno por documento. Ejemplo: JS, CSS, HTML, etc
   const questionRegex = /#### Q\d+[\s\S]*?(?=#### Q\d+|$)/g; // Obtener preguntas
@@ -39,39 +39,45 @@ function convertiraJson(markdown) {
   const correctAnswerRegex = /- \[x\] (.+)/g; // Respuesta correcta
   const incorrectAnswerRegex = /- \[ \] (.+)/g; // Respuestas erróneas
 
-
-
   // Procesar cada bloque de preguntas
   while ((match = questionRegex.exec(markdown)) !== null) {
     const questionBlock = match[0]; // Bloque de preguntas
-    let questionText = questionBlock.replace(/#### Q\d+\s*./, '').split('\n')[0].trim(); // Quitar la primera parte
+    let questionText = questionBlock
+      .replace(/#### Q\d+\s*./, "")
+      .split("\n")[0]
+      .trim(); // Quitar la primera parte
 
     // Bloques de código
-    const codeExamples = [...questionBlock.matchAll(codeExamplesRegex)].map(m => m[1]); // Bloque de ejercicios en preguntas
+    const codeExamples = [...questionBlock.matchAll(codeExamplesRegex)].map(
+      (m) => m[1]
+    ); // Bloque de ejercicios en preguntas
     // Opciones
-    const correctAnswers = [...questionBlock.matchAll(correctAnswerRegex)].map(m => m[1]);
-    const incorrectAnswers = [...questionBlock.matchAll(incorrectAnswerRegex)].map(m => m[1]);
+    const correctAnswers = [...questionBlock.matchAll(correctAnswerRegex)].map(
+      (m) => m[1]
+    );
+    const incorrectAnswers = [
+      ...questionBlock.matchAll(incorrectAnswerRegex),
+    ].map((m) => m[1]);
 
     //llama a la funcion crea el array de opciones, poniendo las tres incorrectas primero y la correcta (True) al final
     const answersOptions = getAnswersOptions(correctAnswers, incorrectAnswers);
     if (!QUESTIONS_CATEGORIES.includes(categories)) {
-      categories = 'other'
-    };
+      categories = [titulo];
+    }
     // Crear el objeto
     const question = {
       categories,
       question: questionText,
       codeExamples,
       answersOptions,
-      urlFont: MARKDOWN_URL
-
+      urlFont: MARKDOWN_URL,
     };
 
     questions.push(question); // Añadimos al array
   }
 
   //Crea la carpeta ListaJson si no existe
-  const path = 'output';
+  const path = "output";
   if (!fs.existsSync(path)) {
     fs.mkdirSync(path);
   }
@@ -84,14 +90,18 @@ function convertiraJson(markdown) {
 
 //funcion que crea el array de opciones, poniendo las tres incorreectas primero y la correcta (True) al final
 function getAnswersOptions(correctAnswers, incorrectAnswers) {
-  const answersOptions = incorrectAnswers.map(a => { return { answer: a, isCorrect: false } }).concat(correctAnswers.map(a => { return { answer: a, isCorrect: true } }));
+  const answersOptions = incorrectAnswers
+    .map((a) => {
+      return { answer: a, isCorrect: false };
+    })
+    .concat(
+      correctAnswers.map((a) => {
+        return { answer: a, isCorrect: true };
+      })
+    );
 
-  return answersOptions
-};
-
-
-
-
+  return answersOptions;
+}
 
 // Ejecutar la descarga y conversión
 descargarMarkdown();
