6f3a597bf7525bc61566d8908a2a2c859b7d0780
MariaChmielinska <maria.anna.chmielinska@gmail.com>
2024-11-20 20:27:24 +0100

    clean before merge to main

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const indexRouter = require("./routes/index");
-const { getRandomQuestion } = require("./services/question.services");
 const bodyParser = require("body-parser");
 
 dotenv.config();
@@ -14,14 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use("/", indexRouter);
-
-app.get("/daily-question", async (req, res) => {
-  // Obtener la pregunta correspondiente al día
-  const question = await getRandomQuestion();
-
-  // Renderizar la página con la pregunta y las opciones
-  res.render("home", { question });
-});
 
 
 const PORT = process.env.PORT || 3000;
