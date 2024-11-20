d943d3c7622870316be328c30b3634d2d9f5d979
dnlslcd <danielsalcedoq@gmail.com>
2024-11-20 21:24:49 +0100

    home design in progress

--- public/script.js
+++ public/script.js
@@ -4,3 +4,18 @@ const navMenu = document.getElementById("nav-menu");
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("show");
 });
+
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primaryGreen: '#41fd5b',
+          secondaryGreen: '#007410',
+          darkGreen: '#35cb49',
+          backgroundGreen: '#003407',
+          errorRed: '#FF7070',
+          secondaryErrorRed: '#DF6666'
+        },
+      },
+    },
+  };
