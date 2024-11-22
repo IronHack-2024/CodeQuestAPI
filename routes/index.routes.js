d66012ad0714f2a20ec353539a475277683205ab
kevmhughes <kevhughes24@hotmail.com>
2024-11-22 03:11:27 +0100

    Add routes to index.routes.js for newsletter functionality

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const indexControllers = require("../controllers/index.controllers");
+const emailServices = require("../services/email.services")
 
 router.get('/', indexControllers.getHome);
 router.get('/about-us', indexControllers.getAboutUs);
@@ -10,5 +11,7 @@ router.get('/daily-question', indexControllers.getDailyQuestion)
 router.get('/export-questions', indexControllers.getTemplateQuestions);
 router.get('/submit-new-question', indexControllers.newQuestionForm);
 router.post('/submit-new-question', indexControllers.createNewQuestion);
+router.post('/subscribe', emailServices.postSubscribe);
+router.get('/sendEmails', emailServices.getSendEmails);
 
 module.exports = router;
