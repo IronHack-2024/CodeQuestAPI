dc9b3e8baf2d643cc63f27266e49428dc5d8e762
Balam Castro <64442582+ekbalam11@users.noreply.github.com>
2024-11-23 14:45:08 +0100

    style: form styling in template-form view and new-question form

--- public/tailwind.js
+++ public/tailwind.js
@@ -4,11 +4,16 @@ tailwind.config = {
         colors: {
           primaryGreen: '#41fd5b',
           secondaryGreen: '#007410',
+          lightGreen: '#acffB7',
           darkGreen: '#35cb49',
           backgroundGreen: '#003407',
           errorRed: '#FF7070',
           secondaryErrorRed: '#DF6666'
         },
+        fontFamily: {
+          oxanium: ['Oxanium', 'sans-serif'],
+          oxygen: ['Oxygen', 'sans-serif'],
+        }
       },
     },
   };
